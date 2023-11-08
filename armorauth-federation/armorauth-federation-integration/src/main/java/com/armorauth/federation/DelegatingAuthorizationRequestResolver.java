b2e40843c07f82e43aa27c8b299a19d1355df296
AutismSuperman <fulinlin9527@outlook.com>
2023-11-08 15:21:09 +0800

    feature: 联合登录配置注册匹配顺序更改

--- armorauth-federation/armorauth-federation-integration/src/main/java/com/armorauth/federation/DelegatingAuthorizationRequestResolver.java
+++ armorauth-federation/armorauth-federation-integration/src/main/java/com/armorauth/federation/DelegatingAuthorizationRequestResolver.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright (c) 2023-present ArmorAuth. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.armorauth.federation;
 
+import com.armorauth.federation.web.FederatedAuthorizationRequestRedirectFilter;
 import com.armorauth.federation.web.converter.OAuth2AuthorizationRequestConverter;
 import com.armorauth.federation.wechat.web.converter.WechatAuthorizationRequestConverter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,7 +39,7 @@ public class DelegatingAuthorizationRequestResolver implements OAuth2Authorizati
     public DelegatingAuthorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository,
                                                   List<OAuth2AuthorizationRequestConverter> authorizationRequestConverters) {
         this(clientRegistrationRepository,
-                OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI, authorizationRequestConverters);
+                FederatedAuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI, authorizationRequestConverters);
     }
 
 
--- armorauth-federation/armorauth-federation-integration/src/main/java/com/armorauth/federation/web/configurers/FederatedLoginConfigurer.java
+++ armorauth-federation/armorauth-federation-integration/src/main/java/com/armorauth/federation/web/configurers/FederatedLoginConfigurer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2023-present ArmorAuth. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.armorauth.federation.web.configurers;
 
 import com.armorauth.federation.authentication.BindUserCheckProvider;
@@ -238,7 +253,7 @@ public class FederatedLoginConfigurer
         } else {
             String authorizationRequestBaseUri = this.authorizationEndpointConfig.authorizationRequestBaseUri;
             if (authorizationRequestBaseUri == null) {
-                authorizationRequestBaseUri = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+                authorizationRequestBaseUri = FederatedAuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
             }
             authorizationRequestFilter = new FederatedAuthorizationRequestRedirectFilter(
                     OAuth2ClientConfigurerUtils.getClientRegistrationRepository(this.getBuilder()),
